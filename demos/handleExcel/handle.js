74263faf718a1bbac9025838c23cdff6e2585ea6
mornki <1048515550@qq.com>
2021-10-21 14:29:18 +0800

    解析excl表格

--- demos/handleExcel/handle.js
+++ demos/handleExcel/handle.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2021-07-22 10:48:37
  * @LastEditors: zhangwen
- * @LastEditTime: 2021-07-22 15:51:04
+ * @LastEditTime: 2021-10-21 14:28:45
  * @FilePath: /DayCode/demos/handleExcel/handle.js
  */
 
@@ -25,11 +25,11 @@ let fnCommon = {
         let path=p.path, rowName=p.rowName, sheetName=p.sheetName, format=p.format;
         let excel_path = __dirname+path;
         let data = await xlsx.parse(fs.readFileSync(excel_path));
-        let sheet_datas = data.map(function(x) {
+            let sheet_datas = data.map(function(x) {
             x.data = x.data.map(function(v,k) {
                 let obj = {};
                 rowName.forEach(function(y, i) {
-                    obj[y] = v[i];
+                    y ? obj[y] = v[i]||'' : '';
                 });
                 return obj;
             })
@@ -44,7 +44,7 @@ let fnCommon = {
             sheet_datas = sheet_datas[0].data;
         }
         sheet_datas = format ? util.inspect(sheet_datas, false, 6) : JSON.stringify(sheet_datas);
-        return sheet_datas;
+        return JSON.stringify(eval("("+sheet_datas+")"));
     },
     /**
      * 写数据到指定文件中
@@ -52,20 +52,47 @@ let fnCommon = {
      * @param {*} sheets_data 
      */
     writeFile: async function(file_path, sheets_data) {
-        return await fs.writeFile(file_path, sheets_data);
+        let datajson = {};
+        try{
+            await fs.writeFile(file_path, sheets_data, function(err, result) {
+                datajson = result;
+            })
+        }catch(e) {
+            console.log("错误:",e);
+        }
+        return datajson;
     }
 }
 
 /**
  * 解析excel数据到指定data.text文件中
  */
-var handleXlsx = async function() {
+/* var handleXlsx = async function() {
     let excel_data = await fnCommon.readXlsx({
         path: '/人员名单.xlsx',
         rowName: ['people_name','people_no','ou','position'],
         format:true,
     });
     await fnCommon.writeFile("data.text", excel_data);
+    console.log("解析成功！")
 }
-handleXlsx()
+handleXlsx() */
 
+
+var handleXlsx1 = async function() {
+    let excel_data = await fnCommon.readXlsx({
+        path: '/索通字段.xlsx',
+        rowName: ['jobgrade_code','jobgrade_name','type','type_name'],
+        format:true,
+    });
+    await fnCommon.writeFile("data.text", excel_data);
+    excel_data = JSON.parse(excel_data);
+    excel_data.shift();
+    let obj = excel_data.reduce(function(c,n,s) {
+        n.jobgrade_code ? c['EmployeeCategoryCode'][n.jobgrade_code] = n.jobgrade_name : '';
+        n.jobgrade_code ? c["JobCode"][n.jobgrade_code] = n.jobgrade_name : '';
+        return c;
+    }, {"EmployeeCategoryCode":{}, "JobCode":{}});
+    console.log("解析成功！",JSON.stringify(obj));
+}
+handleXlsx1()
