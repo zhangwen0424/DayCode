4cd1b100aaf3e8d218608d96d38a14685230d393
mornki <1048515550@qq.com>
2021-12-16 17:25:22 +0800

    utils

--- demos/handleExcel/handle.js
+++ demos/handleExcel/handle.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2021-07-22 10:48:37
  * @LastEditors: zhangwen
- * @LastEditTime: 2021-11-17 16:20:27
+ * @LastEditTime: 2021-12-15 15:18:18
  * @FilePath: /DayCode/demos/handleExcel/handle.js
  */
 
@@ -14,6 +14,7 @@ const fs = require("fs");
 const { isEmpty } = require("lodash");
 const xlsx = require("node-xlsx");
 const util = require("util");
+const color = require("color")
 
 let fnCommon = {
     /**
@@ -25,6 +26,8 @@ let fnCommon = {
     readXlsx: async function(p) {
         let path=p.path, sheetName=p.sheetName, format=p.format;
         let excel_path = __dirname+path;
+        // cellDate将日期转换为正却日期
+        // let data = await xlsx.parse(fs.readFileSync(excel_path), {cellDates: true});
         let data = await xlsx.parse(fs.readFileSync(excel_path));
         // console.log("data",JSON.stringify(data))
         let sheet_datas = data.reduce(function(current, x,index) {
@@ -34,7 +37,7 @@ let fnCommon = {
                 if(!v.length || !k) return c;
                 let obj = {};
                 currentRowName.forEach(function(y, i) {
-                    obj[y] = v[i]||'';
+                    obj[y] = v[i] ? String(v[i]) : '';
                 });
                 c.push(obj)
                 return c;
@@ -45,6 +48,7 @@ let fnCommon = {
         // sheet_datas = format ? util.inspect(sheet_datas, false, 6) : JSON.stringify(sheet_datas);
         // console.log("sheet_datas",sheet_datas)
         // return JSON.stringify(eval("("+sheet_datas+")"));
+        sheetName&&sheet_datas[sheetName] ? sheet_datas = sheet_datas[sheetName] : ''
         return JSON.stringify(sheet_datas);
     },
     /**
@@ -62,7 +66,14 @@ let fnCommon = {
             console.log("错误:",e);
         }
         return datajson;
+    },
+    toDate: (date)=>{
+    if(isNaN(date) && !isNaN(Date.parse(date))){
+      return date;
+    } else {
+    //   return moment(new Date(1900, 0, date)).subtract(1).format('YYYY-MM-DD');
     }
+  }
 }
 
 /**
@@ -109,11 +120,54 @@ var handleXlsx1 = async function() {
 var handleXlsx2 = async function() {
     let excel_data = await fnCommon.readXlsx({
         path: '/十院自定义字段.xlsx',
-        format:true,
     });
     await fnCommon.writeFile("data.text", excel_data);
-    excel_data = JSON.parse(JSON.stringify(excel_data));
-    console.log("excel_data",excel_data)
+    excel_data = JSON.parse(excel_data);
+    let datas = [...excel_data['聘任职称'], ...excel_data['职业、执业证书']].reduce((c,n)=>{
+    // console.log("c,n", c,n)
+        n[0] ? c.push(n[0]) : '';
+        return c;
+    }, []);
+    // console.log("datas",datas);
+
+    // data1
+    let data1 = await fnCommon.readXlsx({
+        path: '/十院自定义字段1.xls',
+        sheetName:"第一页",
+    });
+    await fnCommon.writeFile("data.text", data1);
+    data1 = JSON.parse(data1);
+    data1 = data1.reduce((c,n)=>{
+        if(datas.includes(n[3]) || datas.includes(n[4])){
+            c.push(n[0])
+        }
+        return c;
+    },[]);
+
+    // data2
+    let data2 = await fnCommon.readXlsx({
+        path: '/十院自定义字段2.xls',
+        sheetName:"第一页",
+    });
+    await fnCommon.writeFile("data.text", data2);
+    data2 = JSON.parse(data2);
+    data2 = data2.reduce((c,n)=>{
+        if(datas.includes(n[5])){
+            c.push(n[0])
+        }
+        return c;
+    },[]);
+
+    let total = [...new Set([...data1,...data2])]
+
+
+    console.log("data1", data1, data1.length, [...new Set(data1)].length)
+    console.log("data2", data2, data2.length, [...new Set(data2)].length)
+    console.log("total", JSON.stringify(total), total.length)
+    
+    
+
+
     // let obj = excel_data.reduce(function(c,n,s) {
     //     n.jobgrade_code ? c['JobCode'].push({
     //         code: n.jobgrade_code,
@@ -129,3 +183,13 @@ var handleXlsx2 = async function() {
 }
 handleXlsx2()
 
+var handleXlsx3 = async function() {
+    let excel_data = await fnCommon.readXlsx({
+        path: '/离职人员情况(3).xlsx',
+        format: true,
+    });
+    await fnCommon.writeFile('data.text', excel_data);
+    excel_data = JSON.parse(JSON.stringify(excel_data));
+    console.log("excel——data:", excel_data);
+}
+// handleXlsx3();
